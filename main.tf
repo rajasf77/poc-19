2862e610ddfaa012ac7146d389214cd300f53816
rajasf77 <159114626+rajasf77@users.noreply.github.com>
2026-05-12 15:10:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,12 @@ resource "aws_instance" "poc_server" {
     Name = "POC-19-Production-Server"
   }
 }
+
+resource "aws_instance" "poc_server" {
+  ami           = "ami-0440d3b780d96b29d" 
+  instance_type = "t3.micro" # Changed to t3 for Free Tier compatibility
+
+  tags = {
+    Name = "POC-test-live"
+  }
+}
